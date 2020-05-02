c8640f16fabdd0903b9590035b4590799d135677
harshitg123 <58328462+harshitg123@users.noreply.github.com>
2020-05-02 12:19:46 +0530

    Answer

--- Sets/Symmetric Difference.py
+++ Sets/Symmetric Difference.py
@@ -1,36 +1,3 @@
-/*
-Task:
-Given  sets of integers,  and , print their symmetric difference in ascending order. The term symmetric difference indicates those values that exist in either  or  but do not exist in both.
-
-Input Format:
-
-The first line of input contains an integer, .
-The second line contains  space-separated integers.
-The third line contains an integer, .
-The fourth line contains  space-separated integers.
-
-Output Format:
-
-Output the symmetric difference integers in ascending order, one per line.
-
-Sample Input:
-
-4
-2 4 5 9
-4
-2 4 11 12
-
-Sample Output:
-
-5
-9
-11
-12
-*/
-
-========================================================================================================================================================================
-Sollution:
-========================================================================================================================================================================
 
 import sys
 flag = 0
