d57f2266c03f721fc4ea0e2cdfe2e60e5091889b
harshitg123 <harshit.28.sps@gmail.com>
2020-05-05 15:29:54 +0530

    .txt contain question

---
+++ Basic data types/Nested Lists.py
@@ -0,0 +1,27 @@
+student = list()
+ans = list()
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    student.append([score, name])
+
+student=sorted(student)    
+mn=student[0][0]
+
+i=0
+for check in student:
+    if check[0] == mn:
+        i+=1
+    else:
+        break;
+        
+mn=student[i][0]
+
+for ch in range(i,len(student)):
+    if student[ch][0] == mn:
+        ans.append(student[ch][1])
+    else:
+        break
+     
+for i in sorted(ans):
+    print(i)
