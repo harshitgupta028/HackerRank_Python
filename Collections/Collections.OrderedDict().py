f1aef8c6afbbab54b9c8e3e17810f01d26b6d90f
harshitg123 <harshit.28.sps@gmail.com>
2020-05-09 12:29:19 +0530

    .txt doc contain question

---
+++ Collections/Collections.OrderedDict().py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+
+dic = OrderedDict()
+for _ in range(int(input())):
+    item, space, quantity = input().rpartition(' ')
+    dic[item] = dic.get(item, 0) + int(quantity)
+for i in dic.items():
+    print(*i)
+    
+# .rpartition() function in Python split the given string into three parts. 
+#  rpartition() start looking for separator from right side, till the separator 
+#  is found and return a tuple which contains part of the string before separator, 
+#  argument of the string and the part after the separator.
