4275b79135fbb9c3b9678e332a7fa8a7fc17403b
Harshit gupta <harshit.28.sps@gmail.com>
2020-05-14 15:21:44 +0530

    Answer

--- Basic data types/Tuples.py
+++ Basic data types/Tuples.py
@@ -3,7 +3,6 @@ inp = input()
 inp2 = tuple(map(int, input().split()))
 print(hash(inp2))
 
-
 #                              Properties of hash()
 #     * Objects hashed using hash() are irreversible, leading to loss of information.
 #     * hash() returns hashed value only for immutable objects, hence can be used as an 
