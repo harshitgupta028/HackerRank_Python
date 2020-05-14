1fbfc81a673e5e18da5f4f9b5df04b1777dc12fb
Harshit gupta <harshit.28.sps@gmail.com>
2020-05-14 15:19:59 +0530

    Answer

--- String's/Merge the Tools!.py
+++ String's/Merge the Tools!.py
@@ -17,8 +17,6 @@ def merge_the_tools(string, k):
         p = ilt
         ilt = ilt + k
         
-          
-
-if __name__ == '__main__':
+ if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
